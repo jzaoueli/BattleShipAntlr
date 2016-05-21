793bc169ff8359d0f295c7ecfd46ea682f603571
jzaoueli <zaoueli.jihed@gmail.com>
2016-05-21 20:50:57 +0200

    auf2: set CodeGenerator generate file from CSV file

--- src/auf2/battleship/generator/CodeGenerator.java
+++ src/auf2/battleship/generator/CodeGenerator.java
@@ -1,42 +1,70 @@
 package auf2.battleship.generator;
 
+import auf2.feature.PlayerFieldCSVFeature;
+
 import java.io.*;
 
-public class CodeGenerator {
+public class CodeGenerator extends PlayerFieldCSVFeature {
+
     public static void main(String[] args) throws IOException {
 
-        String generatedStr =
-                "package auf2.battleship.generated;\n\n" +
-                        "class Field {\n\n" +
-                        "   private static String[][] field = new String[][]{\n";
+        String generatedStr = getHeaderStrings();
+        generatedStr += getTableValues();
+        generatedStr = setFooter(generatedStr);
 
+        createAndWriteInFile("src/auf2/battleship/generated/GeneratedField.java", generatedStr);
 
-        try (BufferedReader br = new BufferedReader(new FileReader("src/auf2/battleship/generated/generatedModel.txt"))) {
-            String line;
-            String generatedLine;
-            for (int i = 0; i < 7; i++) {
-                line = br.readLine();
-                generatedLine = "       {";
-                for (int j = 0; j < 7; j++) {
-                    generatedLine = generatedLine + "\"" + line.charAt(j) + "\",";
-                }
-                generatedLine = generatedLine.substring(0, generatedLine.length() - 1) + "},\n";
+    }
 
-                generatedStr = generatedStr + generatedLine;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        generatedStr = generatedStr.substring(0, generatedStr.length() - 2) + "\n   };\n\n"
+    private static String setFooter(String generatedStr) {
+        return generatedStr.substring(0, generatedStr.length() - 2) + "}\n   };\n\n"
                 + "   static String[][] getField(){\n"
                 + "     return field;\n"
                 + "   }\n}";
+    }
 
-        File file = new File("src/auf2/battleship/generated/Field.java");
-        //file.createNewFile();
+    private static String getHeaderStrings() {
+        return "package auf2.battleship.generated;\n\n" +
+                "class GeneratedField {\n\n" +
+                "   private static String[][] field = new String[][]{\n";
+    }
+
+    private static void createAndWriteInFile(String path, String text) throws IOException {
+        File file = new File(path);
         FileWriter writer = new FileWriter(file);
-        writer.write(generatedStr);
+        writer.write(text);
         writer.flush();
         writer.close();
+    }
+
+    private static String getTableValues() throws IOException {
+        setResult();
+        String csvString = getResult();
+        String tempResultString = "       {";
+
+        for (int i = 0; i < csvString.length(); i++) {
+            switch (csvString.charAt(i)) {
+                case 'w':
+                    tempResultString += "\"0\"";
+                    if (csvString.charAt(i + 1) != '\n' && csvString.charAt(i + 1) != '\r') {
+                        tempResultString += ",";
+                    }
+                    break;
+                case 's':
+                    tempResultString += "\"1\"";
+                    if (csvString.charAt(i + 1) != '\n' && csvString.charAt(i + 1) != '\r') {
+                        tempResultString += ",";
+                    }
+                    break;
+                case '\n':
+                    tempResultString += "},\n       {";
+                    break;
+                default:
+                    System.err.println(i + "- this character will not be moved = " + csvString.charAt(i));
+            }
+            System.err.println("actual string : " + tempResultString);
+        }
+        tempResultString += "\"";
+        return tempResultString;
     }
 }
