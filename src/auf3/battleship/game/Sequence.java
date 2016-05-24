870f5c782db3c8ee031898fdf9b98e4e5131770c
jzaoueli <zaoueli.jihed@gmail.com>
2016-05-25 00:24:43 +0200

    auf3: add generated Sequence class 2

---
+++ src/auf3/battleship/game/Sequence.java
@@ -0,0 +1,63 @@
+package auf3.battleship.game;
+
+class Sequence {
+
+    /**
+     * created : 2016/05/25 00:20:37
+     */
+    private static String[] sequence = new String[49];
+
+    static String[] getSequence() {
+        sequence[0] = "A1";
+        sequence[1] = "A2";
+        sequence[2] = "A3";
+        sequence[3] = "A4";
+        sequence[4] = "A5";
+        sequence[5] = "A6";
+        sequence[6] = "A7";
+        sequence[7] = "B1";
+        sequence[8] = "B2";
+        sequence[9] = "B3";
+        sequence[10] = "B4";
+        sequence[11] = "B5";
+        sequence[12] = "B6";
+        sequence[13] = "B7";
+        sequence[14] = "C1";
+        sequence[15] = "C2";
+        sequence[16] = "C3";
+        sequence[17] = "C4";
+        sequence[18] = "C5";
+        sequence[19] = "C6";
+        sequence[20] = "C7";
+        sequence[21] = "D1";
+        sequence[22] = "D2";
+        sequence[23] = "D3";
+        sequence[24] = "D4";
+        sequence[25] = "D5";
+        sequence[26] = "D6";
+        sequence[27] = "D7";
+        sequence[28] = "E1";
+        sequence[29] = "E2";
+        sequence[30] = "E3";
+        sequence[31] = "E4";
+        sequence[32] = "E5";
+        sequence[33] = "E6";
+        sequence[34] = "E7";
+        sequence[35] = "F1";
+        sequence[36] = "F2";
+        sequence[37] = "F3";
+        sequence[38] = "F4";
+        sequence[39] = "F5";
+        sequence[40] = "F6";
+        sequence[41] = "F7";
+        sequence[42] = "G1";
+        sequence[43] = "G2";
+        sequence[44] = "G3";
+        sequence[45] = "G4";
+        sequence[46] = "G5";
+        sequence[47] = "G6";
+        sequence[48] = "G7";
+
+        return sequence;
+    }
+}
